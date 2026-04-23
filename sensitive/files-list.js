2ae38bb814f0c399dbab24e9bbeb104476960b01
MAXIMAX ALWAYS MAXIMIZE <louismaxauto600@gmail.com>
2026-04-23 17:56:06 -0400

    Create files-list.js

---
+++ sensitive/files-list.js
@@ -0,0 +1,30 @@
+// FICHYE: /js/sensitive-files-list.js
+
+// Lis OFISYÈL dosye ki konsidere kòm "sensitive"
+export const SENSITIVE_TYPES = [
+    "application/pdf",
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "text/plain",
+    "audio/mpeg",
+    "video/mp4"
+];
+
+// Lis NON DOSYE ki toujou konsidere SANSIB
+export const SENSITIVE_NAMES = [
+    "id",
+    "passport",
+    "license",
+    "contract",
+    "agreement",
+    "confidential",
+    "private",
+    "secret",
+    "bss1815",
+    "team-san-presyon"
+];
